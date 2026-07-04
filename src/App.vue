d5a37a3cfac3ba6bbff99ff9d1fffbbd24cf49f9
“Satak” <sami.koskivaara@gmail.com>
2026-07-04 14:55:38 +0300

    remove margin

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="daw h-screen flex flex-col" tabindex="0" @keydown="onKeyDown">
-    <div class="flex-1 flex flex-col min-h-0 p-3 gap-3">
+    <div class="flex-1 flex flex-col min-h-0">
       <div class="flex-1 min-h-0">
         <PianoRoll
           :tracks="project.tracks"
